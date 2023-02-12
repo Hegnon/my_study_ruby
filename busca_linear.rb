b2a5b416447949a0e45180e8d10cd75195c29089
Hegnon <hegnondutra@gmail.com>
2023-02-12 17:29:47 -0300

    Craindo Linear Search

---
+++ busca_linear.rb
@@ -0,0 +1,21 @@
+# Criar uma função. 
+
+def busca_linear(lista, pesquisar)
+    # usar o metodo each_with_index para reotrnas os elementos da lista com o indice. 
+   lista.each_with_index do |valor, indice|
+     return indice if valor == pesquisar
+    end
+    return -1
+end
+
+lista = 1,2,3,4,5,6,7,8,9
+pesquisar = 10
+
+resultado = busca_linear(lista, pesquisar)
+
+if resultado != -1
+    puts " O numero #{pesquisar} foi encontrado na posiçao #{resultado}"
+else
+    puts "Não foi encontrado o numero #{pesquisar} na lista"
+end
+    
