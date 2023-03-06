0b1393e78d809db9ca0c623b5030fe3126f9df3c
Hegnon <hegnondutra@gmail.com>
2023-03-06 06:07:24 -0300

    Fim do curso iniciante de classes - Orientação a objeteos

--- conta.rb
+++ conta.rb
@@ -1,15 +1,39 @@
-class Conta
-    #get
-    attr_reader :codigo, :titular, :local
-    #set
-    attr_accessor :placa
+#define alguns metodos para a conta no geral
 
-    #Argumentos
-    def initialize(codigo, titular, local, placa)
-        #Inicialização da variavel.
-        @codigo = codigo
+
+require_relative 'cliente'
+
+class Conta
+    attr_reader :numero, :titular
+
+    attr_accessor :saldo
+
+
+    def initialize(numero, titular, saldo)
+        @numero = numero
         @titular = titular
-        @local = local
-        @placa = placa        
+        @saldo = saldo
+    end
+
+    def sacar(valor)
+        if saldo >= valor
+            self.saldo -= valor
+        else
+            puts "Saldo insuficiente."
+        end    
+    end
+
+
+    def depositar(valor)
+        self.saldo += valor
+    end
+
+    def transferir(conta_destino, valor)
+        if saldo >= valor
+          self.saldo -= valor
+          conta_destino.depositar(valor)
+        else
+          puts "Saldo insuficiente."
+        end
     end
 end
