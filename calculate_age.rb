e58f87e4a4edd6426372fc3f885a7de65ce27ee6
Hegnon <hegnondutra@gmail.com>
2023-03-08 20:07:14 -0300

    algoritmo calcular idade

---
+++ calculate_age.rb
@@ -0,0 +1,11 @@
+require 'date'
+
+data_nacimento = Date.new(1988,11,03)
+data_atual = Date.new.today
+
+idate_anos = ano_atual.year - ano_nacimento.year
+idade_meses = mes_atual.month - mes_nacimento.month
+idade_dias = dia_atual.day - dia_nascomento.day
+
+if 
+
