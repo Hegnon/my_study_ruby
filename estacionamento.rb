15006fdcb7c0fe4bccbae998f29cd53086509e32
Hegnon <hegnondutra@gmail.com>
2023-03-09 07:35:57 -0300

    Estudo classes e metodos

--- estacionamento.rb
+++ estacionamento.rb
@@ -1,7 +1,6 @@
 class Estacionamento
-
-    attr_writer :vaga, :tipo, :nome, :sobrenome
-    attr_accessor :modelo, :placa
+   
+    attr_accessor :modelo, :placa, :vaga, :tipo, :nome, :sobrenome
 
     def initialize(vaga, modelo, placa, tipo, nome, sobrenome)
         @vaga = vaga
@@ -11,6 +10,14 @@ class Estacionamento
         @nome = nome
         @sobrenome = sobrenome
     end
-    
-    
+
+    cliente = Estacionamento.new(1, "palio", "mhp1234", "mensalista","Pedro","Henrique")
+    puts "Cliente: #{cliente.nome}"" #{cliente.sobrenome} - Tipo: #{cliente.tipo}"
+    puts "vaga: #{cliente.vaga}"
+    puts "Carro: #{cliente.modelo} /Placa: #{cliente.placa}"
+        
 end
+    
+   
+    
+    
---
+++ estacionamento_valor.rb
@@ -0,0 +1,8 @@
+class Valor
+
+    def initialize(taxa_hora, taxa_fracao_hora)
+        @taxa_hora = taxa_hora
+        @taxa_fracao_hora = taxa_fracao_hora
+    
+end
+
